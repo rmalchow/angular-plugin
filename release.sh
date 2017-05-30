4eb037c978c0b959fc324c2e2d6d8cba9bcfdfef
ruben malchow <ruben.malchow@gmail.com>
2017-05-30 13:09:37 +0200

    release script

---
+++ release.sh
@@ -0,0 +1,2 @@
+version=`jq '[ (.version | split(".")[0]) ,(.version | split(".")[1]) , ( .version | split(".")[2]  | tonumber + 1 | tostring) ] | join(".")' package.json`
+echo "version is ${version}"
