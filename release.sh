37ae9489f3d7612d5544cd595b7c90acfbc9387f
ruben malchow <ruben.malchow@gmail.com>
2017-05-30 13:11:53 +0200

    hund

--- release.sh
+++ release.sh
@@ -1,2 +1,3 @@
 version=`jq '[ (.version | split(".")[0]) ,(.version | split(".")[1]) , ( .version | split(".")[2]  | tonumber + 1 | tostring) ] | join(".")' package.json`
 echo "version is ${version}"
+
