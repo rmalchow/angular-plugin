c9923744fb1e9b0b157a17f6d0d00902e60fde03
ruben malchow <ruben.malchow@gmail.com>
2017-05-30 13:15:17 +0200

    working on release script

--- release.sh
+++ release.sh
@@ -1,3 +1,17 @@
+git diff --exit-code 2>&1 > /dev/null
+if [ "$?" != "0" ]; then
+	echo "there are unstaged changes"
+	exit -1
+fi
+
+git diff --cached --exit-code  2>&1 > /dev/null
+if [ "$?" != "0" ]; then
+	echo "there are uncommitted changes"
+	exit -2
+fi
+
+
+
 version=`jq '[ (.version | split(".")[0]) ,(.version | split(".")[1]) , ( .version | split(".")[2]  | tonumber + 1 | tostring) ] | join(".")' package.json`
 echo "version is ${version}"
 
