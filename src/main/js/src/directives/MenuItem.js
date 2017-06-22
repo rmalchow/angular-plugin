bfb77f9766e77c9cbf0282f9e452da4a6eb8453e
Ruben Malchow <ruben.malchow@mcon-group.com>
2017-06-22 11:28:20 +0200

    handle empty menu list

--- src/main/js/src/directives/MenuItem.js
+++ src/main/js/src/directives/MenuItem.js
@@ -8,13 +8,19 @@ angular.module("angular-plugin").directive(
 			},
 			link: function(scope, el, attr, ctrl, transclude) {
 				var items = PluginMenuService.get(scope.path);
-				items.forEach(function(each){
-					transclude(function(transEl,transScope) {
-						transScope.item = each;
-						transScope.children = PluginMenuService.get(each.path);
-						el.parent().append(transEl);
+				if(items.length > 0) {
+					items.forEach(function(each){
+						transclude(function(transEl,transScope) {
+							transScope.item = each;
+							transScope.children = PluginMenuService.get(each.path);
+							el.parent().append(transEl);
+						});
 					});
-				});
+				} else {
+					transclude(function(transEl,transScope) {
+						el.hide();
+					});
+				}
 			}
 		}
 	});
