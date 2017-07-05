94af0104482edc62be71202f1a471b81091f9bad
Ruben Malchow <ruben.malchow@mcon-group.com>
2017-07-05 14:00:08 +0200

    fixed release script for macos

--- release.sh
+++ release.sh
@@ -1,22 +1,21 @@
-
 cd `dirname $0`
 git diff --exit-code 2>&1 > /dev/null
 if [ "$?" != "0" ]; then
 	echo "there are unstaged changes"
-#	exit -1
+	exit -1
 fi
 
 git diff --cached --exit-code  2>&1 > /dev/null
 if [ "$?" != "0" ]; then
 	echo "there are uncommitted changes"
-#	exit -2
+	exit -2
 fi
 
 version=`jq -r '[ (.version | split(".")[0]) ,(.version | split(".")[1]) , ( .version | split(".")[2]  | tonumber + 1 | tostring) ] | join(".")' package.json`
 echo "version is ${version}"
 
 package=`jq --arg version ${version} '.version = $version' package.json`
-echo ${package} |jq > package.json
+echo ${package} |jq -rM . > package.json
 
 git tag "${version}"
 git add .
