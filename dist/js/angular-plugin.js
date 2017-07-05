0f7e3e13b01578dbf90a12e299253be4506da92a
Ruben Malchow <ruben.malchow@mcon-group.com>
2017-07-05 14:28:02 +0200

    oh oh main prefix

--- dist/js/angular-plugin.js
+++ dist/js/angular-plugin.js
@@ -1,4 +1,10 @@
-angular.module( 'angular-plugin', ["ngRoute"] );
+angular.module("angular-plugin",["ngRoute"]);
+angular.module("angular-plugin")
+.config(function($routeProvider,$locationProvider) {
+		$locationProvider.hashPrefix('');
+		// rember the route provider so we can retrieve it later ... 
+		angular.module("angular-plugin").routeProvider = $routeProvider;
+});
 
 angular.module("angular-plugin").directive(
 	"includeComponents",
